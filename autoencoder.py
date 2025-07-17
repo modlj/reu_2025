1be202892e6b2a29815a29f46b062596541a3d86
John Modl <modlx010@umn.edu>
2025-07-17 08:00:11 -0500

    Added replacement of extrinsic with intrinsic process

--- autoencoder.py
+++ autoencoder.py
@@ -8,18 +8,20 @@ from skimage.metrics import structural_similarity as ssim
 class Autoencoder(nn.Module):
     def __init__(self, input_channels, latent_dim=128):
         super().__init__()
+        self.encoder_output_size = 2
+        
         self.encoder = nn.Sequential(
             nn.Conv2d(input_channels, 32, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
             nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(64 * ?, latent_dim) # Calculuate input size for linear layer based on env
+            nn.Linear(64 * self.encoder_output_size * self.encoder_output_size, latent_dim) # Calculuate input size for linear layer based on env
         )
         self.decoder = nn.Sequential(
-            nn.Linear(latent_dim, 64 * ?), # Same as above
+            nn.Linear(latent_dim, 64 * self.encoder_output_size * self.encoder_output_size), # Same as above
             nn.ReLU(),
-            nn.Unflatten(1, (64, ?, ?)), # Reshape to match encoder output
+            nn.Unflatten(1, (64, self.encoder_output_size, self.encoder_output_size)), # Reshape to match encoder output
             nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
             nn.ConvTranspose2d(32, input_channels, kernel_size=4, stride=2, padding=1),
@@ -31,5 +33,5 @@ class Autoencoder(nn.Module):
         return reconstructed
 
 def compute_ssim(img1_np, img2_np, data_range=1.0):
-    return ssim(img1_np, img2_np, data_range=data_range, channel_axis=0 if img1_np.shape[0] == 3 else None)
+    return ssim(img1_np, img2_np, data_range=data_range, channel_axis=0)
 
--- im_wrapper.py
+++ im_wrapper.py
@@ -18,4 +18,12 @@ class AutoencoderWrapper(gym.Wrapper):
         with torch.no_grad():
             reconstruction_tensor = self.autoencoder(obs_tensor)
         obs_np = obs_tensor.squeeze(0).cpu().numpy()
+        reconstruction_np = reconstruction_tensor.squeeze(0).cpu().numpy()
+        ssim_score = compute_ssim(obs_np, reconstruction_np, data_range=1.0)
+        intrinsic_reward = (1.0 - ssim_score) * self.intrinsic_reward_scale
+        reward = intrinsic_reward
+
+        info['instrinsic_reward'] = intrinsic_reward
+        info['ssim_score'] = ssim_score
+        return obs, reward, terminated, truncated, info
 
